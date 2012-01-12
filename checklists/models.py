a8992716261ff9316ff0b90d3a250126483f8f59
Giuseppe <denever@users.sf.net>
2012-01-12 17:27:21 +0100

    Aggiunto modello per applicazione checklists

--- checklists/models.py
+++ checklists/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.execptions import ValidationError
 
 # Create your models here.
+
+class CheckList(models.Model):
+    title = models.CharField(max_lenghth=200)
+    date = models.DateTimeField('date')
+
+class BooleanCheck(models.Model):
+    checklist = models.ForeignKey(CheckList)
+    descr = models.CharField(max_length=200)
+    value = models.BooleanField()
+
+class MultipleCheck(models.Model):
+    checklist = models.ForeignKey(CheckList)
+    descr = models.CharField(max_length=200)
+    values = models.CommaSeparatedIntegerField(max_length=20)
+
+def validate_percentage(value):
+    if value < 0 or value > 100:
+        raise ValidationError(u'%s out of range')
+
+class PercentageCheck(models.Model):
+    checklist = models.ForeignKey(CheckList)
+    descr = models.CharField(max_length=200)
+    value = models.SmallIntegerField(validators=[validate_percentage])
+
