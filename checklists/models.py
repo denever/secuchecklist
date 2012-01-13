cc05b9b42ddc44e21782be2701382bde70cd0434
Giuseppe <denever@users.sf.net>
2012-01-13 20:16:38 +0100

    Aggiunta conversione a unicode per migliorare la leggibilità nelle liste

--- checklists/models.py
+++ checklists/models.py
@@ -7,18 +7,30 @@ class Checklist(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateTimeField('date')
 
+    def __unicode__(self):
+        return self.title
+
 class Check(models.Model):
     checklist = models.ForeignKey(Checklist)
     descr = models.CharField(max_length=200)
     answertype = models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return self.descr
+
 class BooleanCheckResult(models.Model):
     check = models.ForeignKey(Check)
     value = models.BooleanField()
 
+    def __unicode__(self):
+        return self.check.descr
+
 class MultipleCheckResult(models.Model):
     check = models.ForeignKey(Check)
     values = models.CommaSeparatedIntegerField(max_length=20)
+
+    def __unicode__(self):
+        return self.check.descr
 
 def validate_percentage(value):
     if value < 0 or value > 100:
@@ -27,3 +39,6 @@ def validate_percentage(value):
 class PercentageCheckResult(models.Model):
     check = models.ForeignKey(Check)
     value = models.SmallIntegerField(validators=[validate_percentage])
+
+    def __unicode__(self):
+        return self.check.descr
