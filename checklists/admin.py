a394f211797f5f6e2020809e6679a3693883825c
Giuseppe <denever@users.sf.net>
2012-01-13 20:17:41 +0100

    Aggiunte modifiche alla pagina di admin delle checklist

--- checklists/admin.py
+++ checklists/admin.py
@@ -1,8 +1,27 @@
 from checklists.models import Checklist, Check
-from checklists.models import BooleanCheckResult, MultipleCheckResult, PercentageCheckResult
+from checklists.models import BooleanCheckResult
+from checklists.models import MultipleCheckResult
+from checklists.models import PercentageCheckResult
+
 from django.contrib import admin
 
-admin.site.register(Checklist)
+class CheckInline(admin.StackedInline):
+    model = Check
+    extra = 10
+
+class ChecklistAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date')
+    list_filter = ['date']
+    search_fields = ['title']
+    date_hierarchy = 'date'
+    
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Date information', {'fields': ['date'], 'classes': ['collapse']}),
+        ]
+    inlines = [CheckInline]
+
+admin.site.register(Checklist, ChecklistAdmin)
 admin.site.register(Check)
 admin.site.register(BooleanCheckResult)
 admin.site.register(MultipleCheckResult)
