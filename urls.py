5a92eaacec049b14326d9ff5ed206ac1429b97b3
Giuseppe <denever@users.sf.net>
2012-02-01 13:37:22 +0100

    Aggiunta sperimentazione checklists view

--- urls.py
+++ urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+                           url(r'^chlst/$', 'checklists.views.index'),
     # url(r'^$', 'secuchecklist.views.home', name='home'),
     # url(r'^secuchecklist/', include('secuchecklist.foo.urls')),
 
@@ -13,5 +14,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+                           url(r'^admin/', include(admin.site.urls)),
+                       
 )
