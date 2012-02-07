8b69cc822de08b9630f8dfd4de1326d83cd3a915
Giuseppe <denever@users.sf.net>
2012-02-07 15:20:18 +0100

    Modificato per utilizzare dentro il template .all

--- customers/views.py
+++ customers/views.py
@@ -19,9 +19,8 @@ def customer_detail(request, customer_id):
     #         })
     # return HttpResponse(tmpl.render(cnt))
     cc = get_object_or_404(CustomerCompany, pk=customer_id)
-    staff = cc.staff_set.all()
-    return render_to_response('customers/customer_detail.html',
-                              {'staff_list': staff, 'company': cc})
+    return render_to_response('customers/customer_detail.html', {'company': cc})
+
 def staff_detail(request, staff_id):
 #    s = Staff.objects.get(id=staff_id)
     s = get_object_or_404(Staff, pk=staff_id)
