ba0f59b75110960a0cc37bd5302f7a39c2ab6772
Giuseppe <denever@users.sf.net>
2012-02-08 10:28:50 +0100

    Utilizzate le view generiche

--- customers/urls.py
+++ customers/urls.py
@@ -1,7 +1,16 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import DetailView, ListView
+from customers.models import CustomerCompany, Staff
 
 urlpatterns = patterns('customers.views',
-                        url(r'^$', 'list'),
-                        url(r'^(?P<customer_id>\d+)/$', 'customer_detail'),
-                        url(r'^staff/(?P<staff_id>\d+)/$', 'staff_detail'),
-                        )
+                       url(r'^$', ListView.as_view(queryset=CustomerCompany.objects.all(),
+                                                   context_object_name='cc_list')),
+                       url(r'^(?P<pk>\d+)/$', DetailView.as_view(model=CustomerCompany,
+                                                                 context_object_name='company')),
+                       url(r'^staff/(?P<pk>\d+)/$', DetailView.as_view(model=Staff,
+                                                                 context_object_name='staff')),
+                       )
+
+#                        url(r'^$', 'list'),
+#                        url(r'^(?P<customer_id>\d+)/$', 'customer_detail'),
+#                        url(r'^staff/(?P<staff_id>\d+)/$', 'staff_detail'),
