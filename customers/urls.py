15bf5fe424d240aaecbf77f408e13450e9081d42
Giuseppe <denever@users.sf.net>
2012-02-10 18:22:48 +0100

    Aggiunto login required per customers

--- customers/urls.py
+++ customers/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.auth.decorators import login_required, permission_required
 from customers.models import CustomerCompany, Staff
 from customers.views import CustomerCompanyYearView
 from customers.views import CustomerCompanyListView
 from customers.views import CustomerCompanyDetailView
 from customers.views import StaffDetailView
-from django.views.generic import DetailView, ListView
 
 urlpatterns = patterns('customers.views',
-                       url(r'^$', CustomerCompanyListView.as_view()),
-                       url(r'^(?P<pk>\d+)/$', CustomerCompanyDetailView.as_view()),
-                       url(r'^staff/(?P<pk>\d+)/$', StaffDetailView.as_view()),
-                       url(r'year/(?P<year>\d{4})/$', CustomerCompanyYearView.as_view()),
+                       url(r'^$', login_required(CustomerCompanyListView.as_view())),
+                       url(r'^(?P<pk>\d+)/$', login_required(CustomerCompanyDetailView.as_view())),
+                       url(r'^staff/(?P<pk>\d+)/$', login_required(StaffDetailView.as_view())),
+                       url(r'year/(?P<year>\d{4})/$', login_required(CustomerCompanyYearView.as_view())),
                        )
