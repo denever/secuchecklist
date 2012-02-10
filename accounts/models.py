1478c1bf84ef91396bae29447671be644065b448
Giuseppe <denever@users.sf.net>
2012-02-10 12:15:41 +0100

    Unicode cast per userprofile

--- accounts/models.py
+++ accounts/models.py
@@ -5,4 +5,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
+
+    def __unicode__(self):
+        return self.user.username
     
