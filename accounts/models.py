e28bf455e5ebc9a4989e73a28a060020dd888af1
Giuseppe <denever@users.sf.net>
2012-02-10 18:22:29 +0100

    Aggiunti commenti

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
+    # checklist suspendend
+    # checklist completed
+    # customercompany_set tutte le compagnie da te registrate
 
     def __unicode__(self):
         return self.user.username
