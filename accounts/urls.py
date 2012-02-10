df5d2829a16afbce271a41eacb2a1fc1a7e13daf
Giuseppe <denever@users.sf.net>
2012-02-10 11:39:48 +0100

    Aggiunti hook per cambio password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
+from accounts.views import ProfileView
 
 urlpatterns = patterns('',
-                       # (r'^profile/$', 'django.contrib.auth.views.profile',
-                       #  {'template_name': 'accounts/profile.html'}),
+                       (r'^profile/$', ProfileView.as_view(),
+                        {'template_name': 'accounts/profile.html'}),
                        (r'^login/$', 'django.contrib.auth.views.login',
                         {'template_name': 'accounts/login.html'}),
                        (r'^logout/$', 'django.contrib.auth.views.logout',
                         {'template_name': 'accounts/logout.html'}),
+                       (r'^changepasswd/$', 'django.contrib.auth.views.password_change',
+                        {'template_name': 'accounts/password_change_form.html'}),
+                       (r'^changedone/$', 'django.contrib.auth.views.password_change_done',
+                        {'template_name': 'accounts/password_change_done.html'}),
 )
