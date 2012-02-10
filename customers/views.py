9d6195a575318ed83ac2ea69c13d3acecaea5930
Giuseppe <denever@users.sf.net>
2012-02-10 13:17:52 +0100

    Riscritte le view per utilizzare le view class based

--- customers/views.py
+++ customers/views.py
@@ -1,28 +1,19 @@
 # Create your views here.
-from django.shortcuts import render_to_response, get_object_or_404
+from django.views.generic import DetailView, ListView, YearArchiveView
 from customers.models import *
 
-def list(request):
-    ccs = CustomerCompany.objects.all().order_by('-record_date')
-    # tmpl = loader.get_template('customers/customers_list.html')
-    # cnt = Context({'cc_list': ccs})
-    # return HttpResponse(tmpl.render(cnt))
-    return render_to_response('customers/customercompany_list.html', {'cc_list': ccs})
+class CustomerCompanyYearView(YearArchiveView):
+    queryset = CustomerCompany.objects.all()
+    date_field = 'record_date'
+    make_object_list = True
 
-def customer_detail(request, customer_id):
-    # cc = CustomerCompany.objects.get(id=customer_id)
-    # staff = cc.staff_set.all()
-    # tmpl = loader.get_template('customers/customer_detail.html')
-    # cnt = Context({
-    #         'staff_list': staff,
-    #         'company': cc
-    #         })
-    # return HttpResponse(tmpl.render(cnt))
-    cc = get_object_or_404(CustomerCompany, pk=customer_id)
-    return render_to_response('customers/customercompany_detail.html', {'company': cc})
+class CustomerCompanyListView(ListView):
+    queryset = CustomerCompany.objects.all()
 
-def staff_detail(request, staff_id):
-#    s = Staff.objects.get(id=staff_id)
-    s = get_object_or_404(Staff, pk=staff_id)
-    return render_to_response('customers/staff_detail.html',
-                              {'staff': s})
+class CustomerCompanyDetailView(DetailView):
+    model=CustomerCompany
+    context_object_name = 'company'
+
+class StaffDetailView(DetailView):
+    model=Staff
+    context_object_name = 'staff'    
