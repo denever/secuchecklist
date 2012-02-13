b3a019ecb50c1276769f0ce6c0bd6fa79c8746de
Giuseppe <denever@users.sf.net>
2012-02-13 15:28:53 +0100

    Riorganizzato ordine dei campi ed aggiunta label

--- customers/forms.py
+++ customers/forms.py
@@ -5,17 +5,36 @@ from customers.models import CustomerCompany, Staff, Nationality
 from django.contrib.admin import widgets
 
 class CustomerCompanyForm(forms.ModelForm):
-    tax_code = ITSocialSecurityNumberField()
-    vat_code = ITVatNumberField()
-    phone = ITPhoneNumberField()
-    fax = ITPhoneNumberField()
+    tax_code = ITSocialSecurityNumberField(label='Codice Fiscale')
+    vat_code = ITVatNumberField(label='Partita IVA')
+    phone = ITPhoneNumberField(label='Telefono')
+    fax = ITPhoneNumberField(label='Fax')
 
     class Meta:
         model = CustomerCompany
-        exclude = ('record_by')
-
+        fields = ('record_by',
+                  'firm',
+                  'phone',
+                  'fax',
+                  'email',
+                  'registered_office',
+                  'settlement',
+                  'ciiaa',
+                  'tax_code',
+                  'vat_code',
+                  'inail_pos',
+                  'inps_pos',
+                  'ccnl',
+                  'ateco_sector',
+                  'certifications',
+                  'settlement_size',
+                  'cpi',
+                  'machine_use',
+                  'dangerous_substances',
+                  'health_surveillance')
+        
 class StaffForm(forms.ModelForm):
-    phone = ITPhoneNumberField(required=False)
+    phone = ITPhoneNumberField(label='Telefono', required=False)
 
     class Meta:
         model = Staff
