769d4f65942aebcc000bf891ab29a4204167b9fa
Giuseppe <denever@users.sf.net>
2012-02-13 12:44:36 +0100

    Utilizzato date widget da admin nello staff create form

--- customers/forms.py
+++ customers/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.localflavor.it.forms import ITSocialSecurityNumberField, ITVatNumberField
 from itphone_field import ITPhoneNumberField
 from customers.models import CustomerCompany, Staff
+from django.contrib.admin import widgets
 
 class CustomerCompanyForm(forms.ModelForm):
     tax_code = ITSocialSecurityNumberField()
@@ -18,3 +19,6 @@ class StaffForm(forms.ModelForm):
     
     class Meta:
         model = Staff
+        widgets = {'birth_date': widgets.AdminDateWidget(),
+                   'employ_date':widgets.AdminDateWidget(),
+                   }
