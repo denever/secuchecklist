e86e8338b0a325088890aa6d0d78c374dd34e965
Giuseppe <denever@users.sf.net>
2012-02-13 12:12:29 +0100

    Personalizzate le form utilizzando campi dedicati per partita iva e numero di telefono e codice fiscale

--- customers/forms.py
+++ customers/forms.py
@@ -1,6 +1,20 @@
 from django import forms
-from customers.models import CustomerCompany
+from django.contrib.localflavor.it.forms import ITSocialSecurityNumberField, ITVatNumberField
+from itphone_field import ITPhoneNumberField
+from customers.models import CustomerCompany, Staff
 
 class CustomerCompanyForm(forms.ModelForm):
+    tax_code = ITSocialSecurityNumberField()
+    vat_code = ITVatNumberField()
+    phone = ITPhoneNumberField()
+    fax = ITPhoneNumberField()
+
     class Meta:
         model = CustomerCompany
+        exclude = ('record_by')
+
+class StaffForm(forms.ModelForm):
+    phone = ITPhoneNumberField()
+    
+    class Meta:
+        model = Staff
