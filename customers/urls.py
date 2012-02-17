2170549a3a11b3ca0a3f140a72090e0a4022fcb1
Giuseppe <denever@users.sf.net>
2012-02-17 17:16:28 +0100

    Aggiunti url alle viste companysecurityduty

--- customers/urls.py
+++ customers/urls.py
@@ -20,76 +20,89 @@ from customers.views import WorkingEnvironmentEditView
 from customers.views import DepartmentCreateView
 from customers.views import DepartmentDetailView
 
+from customers.views import CompanySecurityDutyCreateView
+from customers.views import CompanySecurityDutyListView
+
 urlpatterns = patterns('customers.views',
-		       url(r'^$', login_required(CustomerCompanyListView.as_view()),
-			   name='customers'),
+                       url(r'^$', login_required(CustomerCompanyListView.as_view()),
+                           name='customers'),
 
-		       url(r'^(?P<pk>\d+)/$',
-			   login_required(CustomerCompanyDetailView.as_view()),
-			   name = 'company-detail'),
+                       url(r'^(?P<pk>\d+)/$',
+                           login_required(CustomerCompanyDetailView.as_view()),
+                           name = 'company-detail'),
 
-		       # url(r'^staff/(?P<pk>\d+)/$',
-		       #     login_required(StaffDetailView.as_view()),
-		       #     name = 'staff-detail'
-		       #     ),
+                       # url(r'^staff/(?P<pk>\d+)/$',
+                       #     login_required(StaffDetailView.as_view()),
+                       #     name = 'staff-detail'
+                       #     ),
 
-		       url(r'^(?P<company>\d+)/staff/(?P<pk>\d+)/$',
-			   login_required(StaffDetailView.as_view()),
-			   name = 'staff-detail'
-			   ),
+                       url(r'^(?P<company>\d+)/staff/(?P<pk>\d+)/$',
+                           login_required(StaffDetailView.as_view()),
+                           name = 'staff-detail'
+                           ),
 
-		       url(r'year/(?P<year>\d{4})/$',
-			   login_required(CustomerCompanyYearView.as_view()),
-			   name = 'company-year'
-			   ),
+                       url(r'year/(?P<year>\d{4})/$',
+                           login_required(CustomerCompanyYearView.as_view()),
+                           name = 'company-year'
+                           ),
 
-		       url(r'^create/$',
-			   login_required(CustomerCompanyCreateView.as_view()),
-			   name = 'company-create'
-			   ),
+                       url(r'^create/$',
+                           login_required(CustomerCompanyCreateView.as_view()),
+                           name = 'company-create'
+                           ),
 
-		       url(r'^(?P<company>\d+)/add_staff/$',
-			   login_required(StaffCreateView.as_view()),
-			   name = 'staff-add'
-			   ),
+                       url(r'^(?P<company>\d+)/add_staff/$',
+                           login_required(StaffCreateView.as_view()),
+                           name = 'staff-add'
+                           ),
 
-		       url(r'^(?P<pk>\d+)/update/$',
-			   login_required(CustomerCompanyUpdateView.as_view()),
-			   name = 'company-edit'
-			   ),
+                       url(r'^(?P<pk>\d+)/update/$',
+                           login_required(CustomerCompanyUpdateView.as_view()),
+                           name = 'company-edit'
+                           ),
 
-		       url(r'^update_staff/(?P<pk>\d+)/$',
-			   login_required(StaffUpdateView.as_view()),
-			   name = 'staff-edit'
-			   ),
+                       url(r'^update_staff/(?P<pk>\d+)/$',
+                           login_required(StaffUpdateView.as_view()),
+                           name = 'staff-edit'
+                           ),
 
-		       url(r'^delete/(?P<pk>\d+)/$',
-			   login_required(CustomerCompanyDeleteView.as_view()),
-			   name = 'delete-company'
-			   ),
+                       url(r'^delete/(?P<pk>\d+)/$',
+                           login_required(CustomerCompanyDeleteView.as_view()),
+                           name = 'delete-company'
+                           ),
 
-		       url(r'^delete_staff/(?P<pk>\d+)/$',
-			   login_required(StaffDeleteView.as_view()),
-			   name = 'staff-delete'
-			   ),
+                       url(r'^delete_staff/(?P<pk>\d+)/$',
+                           login_required(StaffDeleteView.as_view()),
+                           name = 'staff-delete'
+                           ),
 
-		       url(r'^(?P<pk>\d+)/working_env/$',
-			   login_required(WorkingEnvironmentEditView.as_view()),
-			   name = 'set-working-env'
-			   ),
+                       url(r'^(?P<pk>\d+)/working_env/$',
+                           login_required(WorkingEnvironmentEditView.as_view()),
+                           name = 'set-working-env'
+                           ),
 
-		       url(r'^(?P<company>\d+)/add_department/$',
-			   login_required(DepartmentCreateView.as_view()),
-			   name = 'department-add'
-			   ),
+                       url(r'^(?P<company>\d+)/add_department/$',
+                           login_required(DepartmentCreateView.as_view()),
+                           name = 'department-add'
+                           ),
 
-		       url(r'^(?P<company>\d+)/department/(?P<pk>\d+)/$',
-			   login_required(DepartmentDetailView.as_view()),
-			   name = 'department-detail'
-			   ),
+                       url(r'^(?P<company>\d+)/department/(?P<pk>\d+)/$',
+                           login_required(DepartmentDetailView.as_view()),
+                           name = 'department-detail'
+                           ),
 
-		       url(r'^(?P<company>\d+)/staff/$',
-			   login_required(StaffListView.as_view()),
-			   name = 'staff-list'
-			   ),
-		       )
+                       url(r'^(?P<company>\d+)/staff/$',
+                           login_required(StaffListView.as_view()),
+                           name = 'staff-list'
+                           ),
+
+                       url(r'^(?P<company>\d+)/add_secduty/$',
+                           login_required(CompanySecurityDutyCreateView.as_view()),
+                           name = 'companysecurityduty-add'
+                           ),
+
+                       url(r'^(?P<company>\d+)/secduties/$',
+                           login_required(CompanySecurityDutyListView.as_view()),
+                           name = 'companysecurityduty-list'
+                           ),
+                       )
