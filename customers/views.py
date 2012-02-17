5f6c82cf0f0b5d2907c64f9d76a38159ef9a1459
Giuseppe <denever@users.sf.net>
2012-02-17 17:15:46 +0100

    Whitespace cleanup, riorganizzato ordine, aggiunte viste per CompanySecurityDuty

--- customers/views.py
+++ customers/views.py
@@ -12,6 +12,7 @@ from customers.forms import CustomerCompanyForm
 from customers.forms import StaffForm
 from customers.forms import WorkingEnvironmentForm
 from customers.forms import DepartmentForm
+from customers.forms import CompanySecurityDutyForm
 
 class CustomerCompanyYearView(YearArchiveView):
     queryset = CustomerCompany.objects.all()
@@ -32,9 +33,9 @@ class CustomerCompanyCreateView(CreateView):
     success_url = '/customers/'
 
     def form_valid(self, form):
-	self.company = form.save(commit=False)
-	self.company.record_by = self.request.user.get_profile()
-	return super(CustomerCompanyCreateView, self).form_valid(form)
+        self.company = form.save(commit=False)
+        self.company.record_by = self.request.user.get_profile()
+        return super(CustomerCompanyCreateView, self).form_valid(form)
 
 class CustomerCompanyUpdateView(UpdateView):
     model = CustomerCompany
@@ -44,8 +45,8 @@ class CustomerCompanyUpdateView(UpdateView):
     context_object_name = 'company'
 
     def form_valid(self, form):
-	self.success_url = reverse('company-detail', args=self.kwargs['pk'])
-	return super(CustomerCompanyUpdateView, self).form_valid(form)
+        self.success_url = reverse('company-detail', args=self.kwargs['pk'])
+        return super(CustomerCompanyUpdateView, self).form_valid(form)
 
 class CustomerCompanyDeleteView(DeleteView):
     model = CustomerCompany
@@ -62,25 +63,37 @@ class StaffCreateView(CreateView):
     #     self.initial['company'] = self.kwargs['company']
     #     return self.initial
     def get_context_data(self, **kwargs):
-	context = super(StaffCreateView, self).get_context_data(**kwargs)
-	context['company'] = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
-	return context
+        context = super(StaffCreateView, self).get_context_data(**kwargs)
+        context['company'] = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
+        return context
 
     def form_valid(self, form):
-	self.staff = form.save(commit=False)
-	self.staff.record_by = self.request.user.get_profile()
-	self.staff.company = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
-	self.success_url = reverse('staff-list', args=self.kwargs['company'])
-	return super(StaffCreateView, self).form_valid(form)
+        self.staff = form.save(commit=False)
+        self.staff.record_by = self.request.user.get_profile()
+        self.staff.company = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
+        self.success_url = reverse('staff-list', args=self.kwargs['company'])
+        return super(StaffCreateView, self).form_valid(form)
+
+class StaffListView(ListView):
+    context_object_name = 'staff_set'
+
+    def get_queryset(self):
+        company = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
+        return company.staff_set.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(StaffListView, self).get_context_data(**kwargs)
+        context['company'] = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
+        return context
 
 class StaffDetailView(DetailView):
     model = Staff
     context_object_name = 'staff'
 
     def get_context_data(self, **kwargs):
-	context = super(StaffDetailView, self).get_context_data(**kwargs)
-	context['company'] = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
-	return context
+        context = super(StaffDetailView, self).get_context_data(**kwargs)
+        context['company'] = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
+        return context
 
 class StaffUpdateView(UpdateView):
     model = Staff
@@ -89,8 +102,8 @@ class StaffUpdateView(UpdateView):
     success_url = '/customers/'
 
     def form_valid(self, form):
-	self.success_url = reverse('staff-detail', args=self.kwargs['company'])
-	return super(StaffUpdateView, self).form_valid(form)
+        self.success_url = reverse('staff-detail', args=self.kwargs['company'])
+        return super(StaffUpdateView, self).form_valid(form)
 
 class StaffDeleteView(DeleteView):
     model = Staff
@@ -106,33 +119,33 @@ class WorkingEnvironmentEditView(UpdateView):
     context_object_name = 'company'
 
     def form_valid(self, form):
-	self.success_url = reverse('set-working-env', args=[self.kwargs['pk']])
-	return super(WorkingEnvironmentEditView, self).form_valid(form)
+        self.success_url = reverse('set-working-env', args=[self.kwargs['pk']])
+        return super(WorkingEnvironmentEditView, self).form_valid(form)
 
 class DepartmentCreateView(CreateView):
     form_class = DepartmentForm
     template_name = 'customers/department_create_form.html'
 
     def form_valid(self, form):
-	self.department = form.save(commit=False)
-	self.department.record_by = self.request.user.get_profile()
-	self.department.company = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
-	self.success_url = reverse('set-working-env', args=self.kwargs['company'])
-	return super(DepartmentCreateView, self).form_valid(form)
+        self.department = form.save(commit=False)
+        self.department.record_by = self.request.user.get_profile()
+        self.department.company = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
+        self.success_url = reverse('set-working-env', args=self.kwargs['company'])
+        return super(DepartmentCreateView, self).form_valid(form)
 
     def get_context_data(self, **kwargs):
-	context = super(DepartmentCreateView, self).get_context_data(**kwargs)
-	context['company'] = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
-	return context
+        context = super(DepartmentCreateView, self).get_context_data(**kwargs)
+        context['company'] = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
+        return context
 
 class DepartmentDetailView(DetailView):
     model = Department
     context_object_name = 'department'
 
     def get_context_data(self, **kwargs):
-	context = super(DepartmentDetailView, self).get_context_data(**kwargs)
-	context['company'] = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
-	return context
+        context = super(DepartmentDetailView, self).get_context_data(**kwargs)
+        context['company'] = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
+        return context
 
 class DepartmentUpdateView(UpdateView):
     model = Department
@@ -141,8 +154,8 @@ class DepartmentUpdateView(UpdateView):
     success_url = '/customers/'
 
     def form_valid(self, form):
-	self.success_url = reverse('department-detail', args=self.kwargs['company'])
-	return super(DepartmentUpdateView, self).form_valid(form)
+        self.success_url = reverse('department-detail', args=self.kwargs['company'])
+        return super(DepartmentUpdateView, self).form_valid(form)
 
 class DepartmentDeleteView(DeleteView):
     model = Department
@@ -150,14 +163,31 @@ class DepartmentDeleteView(DeleteView):
     template_name = 'customers/department_delete_form.html'
     success_url = '/customers/'
 
-class StaffListView(ListView):
-    context_object_name = 'staff_set'
-
-    def get_queryset(self):
-	company = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
-	return company.staff_set.all()
+class CompanySecurityDutyCreateView(CreateView):
+    form_class = CompanySecurityDutyForm
+    template_name = 'customers/companysecurityduty_create_form.html'
 
     def get_context_data(self, **kwargs):
-	context = super(StaffListView, self).get_context_data(**kwargs)
-	context['company'] = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
-	return context
+        context = super(CompanySecurityDutyCreateView, self).get_context_data(**kwargs)
+        context['company'] = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
+        return context
+
+    def form_valid(self, form):
+        self.companysecurityduty = form.save(commit=False)
+        self.companysecurityduty.record_by = self.request.user.get_profile()
+        self.companysecurityduty.company = get_object_or_404(CustomerCompany,
+                                                             id=self.kwargs['company'])
+        self.success_url = reverse('companysecurityduty-list', args=self.kwargs['company'])
+        return super(CompanySecurityDutyCreateView, self).form_valid(form)
+
+class CompanySecurityDutyListView(ListView):
+    context_object_name = 'companysecurityduty_set'
+
+    def get_queryset(self):
+        company = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
+        return company.companysecurityduty_set.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(CompanySecurityDutyListView, self).get_context_data(**kwargs)
+        context['company'] = get_object_or_404(CustomerCompany, id=self.kwargs['company'])
+        return context
