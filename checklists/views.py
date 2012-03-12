e2eb1c93d2b6a73c00a2055c3a695fdbdd38e32c
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-03-12 10:17:21 +0100

    Removed old checklist app

--- checklists/views.py
+++ checklists/views.py
@@ -1,15 +1,1 @@
-from checklists.models import Azienda
-from django.http import HttpResponse
-
 # Create your views here.
-
-def index(request):
-    aziende = Azienda.objects.all().order_by('-data_registrazione')
-    output = ','.join([str(azienda) for azienda in aziende])
-    return HttpResponse(output)
-
-def azienda(request, azienda_id):
-    return HttpResponse('Mostra azienda e sue checklist')
-
-def reparti(request, azienda_id):
-    return HttpResponse('Mostra i reparti di una azienda')
