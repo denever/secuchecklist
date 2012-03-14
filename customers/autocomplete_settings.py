b0be954cf2762184f93845d3f4a2ab5fff8e2137
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-03-14 09:23:07 +0100

    Aggiunte autocomplete per township, zipcode, province

--- customers/autocomplete_settings.py
+++ customers/autocomplete_settings.py
@@ -1,7 +1,8 @@
 from autocomplete.views import autocomplete
 # Settings for django-autocomplete
+from django.db.models import Count
 
-from customers.models import AtecoSector, CPISettlement
+from customers.models import AtecoSector, CPISettlement, TownShip
 from autocomplete.views import AutocompleteSettings
 
 class AtecoSectorAutocomplete(AutocompleteSettings):
@@ -14,5 +15,33 @@ class CPISettlementAutocomplete(AutocompleteSettings):
     search_fields = ('name',)
     login_required = True
 
+class TownShipNameAutocomplete(AutocompleteSettings):
+    queryset = TownShip.objects.all()
+    search_fields = ('^name',)
+    value = 'name'
+    label = 'name'
+    key = 'name'
+    login_required = True
+
+class ZipCodeAutocomplete(AutocompleteSettings):
+    queryset = TownShip.objects.all()
+    search_fields = ('^zipcode',)
+    value = 'zipcode'
+    label = 'zipcode'
+    key = 'zipcode'
+    login_required = True
+
+class ProvinceAutocomplete(AutocompleteSettings):
+    queryset = TownShip.objects.all()
+    search_fields = ('province',)
+    value = 'province'
+    label = 'province'
+    key = 'province'
+    login_required = True
+
+
 autocomplete.register('atecosector', AtecoSectorAutocomplete)
 autocomplete.register('cpisettlement', CPISettlementAutocomplete)
+autocomplete.register('town', TownShipNameAutocomplete)
+autocomplete.register('zipcode', ZipCodeAutocomplete)
+autocomplete.register('province', ProvinceAutocomplete)
