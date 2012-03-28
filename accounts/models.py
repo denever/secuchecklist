b8b771d0e760719e7eb790117efca647c06c3d50
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-03-28 10:31:19 +0200

    Prima bozza di ActivityLogger

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,17 @@
+# encoding: utf-8
 from django.db import models
 
-# Create your models here.
+# importing User for UserProfile
 from django.contrib.auth.models import User
+
+# importing for signals and logging
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.utils.encoding import force_unicode
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+
+CREATE, EDIT, DELETE = 'Create', 'Edit', 'Delete'
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
@@ -11,6 +21,41 @@ class UserProfile(models.Model):
 
     def __unicode__(self):
         return self.user.username
-    
+
 class Activity(models.Model):
-    date = models.DateTimeField('Data registrazione', auto_now_add=True)
+    userprofile = models.ForeignKey(UserProfile)
+    date = models.DateTimeField('Data attività', auto_now_add=True)
+    action = models.CharField('Attività', max_length=6)
+    object_id = models.PositiveIntegerField()
+    object_repr = models.CharField('Nome Oggetto', max_length=50)
+    content_type = models.ForeignKey(ContentType)
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+
+    def __unicode__(self):
+        return '%s %s %s %s' % (self.date, self.userprofile, self.action, self.object_repr)
+
+@receiver(post_save)
+def post_save_cb(sender, **kwargs):
+    print sender
+    print kwargs
+    instance = kwargs['instance']
+    if hasattr(instance, 'record_by'):
+        a = Activity(userprofile=instance.record_by,
+                     action=CREATE,
+                     object_repr=force_unicode(instance),
+                     content_object=instance
+                     )
+        a.save()
+
+@receiver(post_delete)
+def post_delete_cb(sender, **kwargs):
+    print sender
+    print kwargs
+    instance = kwargs['instance']
+    if hasattr(instance, 'record_by'):
+        a = Activity(userprofile = instance.record_by,
+                     action = DELETE,
+                     object_repr = force_unicode(instance),
+                     content_object = instance
+                     )
+        a.save()
