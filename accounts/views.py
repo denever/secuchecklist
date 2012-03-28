1a9eceb2720bed98299cf554e6e320e56dfc093a
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-03-28 17:17:51 +0200

    Aggiunta la vista per le attività

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,17 @@
 # Create your views here.
+from django.shortcuts import get_object_or_404
+
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from accounts.models import UserProfile
 
 class ProfileView(TemplateView):
     template_name = "accounts/profile.html"
+
+class ActivityListView(ListView):
+    context_object_name = 'activities'
+
+    def get_queryset(self):
+        user_profile = self.request.user.get_profile()
+        return user_profile.activity_set.all().order_by('date')
