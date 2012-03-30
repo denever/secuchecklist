8ee5a8927888b3d645c1be19125ef6b97707f394
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-03-30 13:59:01 +0200

    Tradotto in inglese

--- customers/forms.py
+++ customers/forms.py
@@ -15,75 +15,75 @@ from customers.models import CompanySecurityDuty
 from autocomplete import widgets as autocomplete_widgets
 
 class CustomerCompanyForm(forms.ModelForm):
-    tax_code = ITVatNumberField(label=_('Codice Fiscale'))
-    vat_code = ITVatNumberField(label=_('Partita IVA'))
-    phone = ITPhoneNumberField(label=_('Telefono'))
+    tax_code = ITVatNumberField(label=_('Tax code'))
+    vat_code = ITVatNumberField(label=_('Vat code'))
+    phone = ITPhoneNumberField(label=_('Phone'))
     fax = ITPhoneNumberField(label=_('Fax'))
 
     class Meta:
-	model = CustomerCompany
-	fields = (
-		  'firm',
-		  'phone',
-		  'fax',
-		  'email',
-		  'registered_office',
-		  'settlement',
-		  'ciiaa',
-		  'tax_code',
-		  'vat_code',
-		  'inail_pos',
-		  'inps_pos',
-		  'ccnl',
-		  'ateco_sector',
-		  'certifications',
-		  'settlement_size',
-		  'cpi',
-		  'machine_use',
-		  'dangerous_substances',
-		  'health_surveillance')
+        model = CustomerCompany
+        fields = (
+                  'firm',
+                  'phone',
+                  'fax',
+                  'email',
+                  'registered_office',
+                  'settlement',
+                  'ciiaa',
+                  'tax_code',
+                  'vat_code',
+                  'inail_pos',
+                  'inps_pos',
+                  'ccnl',
+                  'ateco_sector',
+                  'certifications',
+                  'settlement_size',
+                  'cpi',
+                  'machine_use',
+                  'dangerous_substances',
+                  'health_surveillance')
 
-	widgets = {'ateco_sector': autocomplete_widgets.AutocompleteWidget('atecosector'),
-		   'cpi': autocomplete_widgets.AutocompleteWidget('cpisettlement')
-		   }
+        widgets = {'ateco_sector': autocomplete_widgets.AutocompleteWidget('atecosector'),
+                   'cpi': autocomplete_widgets.AutocompleteWidget('cpisettlement')
+                   }
 
 class StaffForm(forms.ModelForm):
-    phone = ITPhoneNumberField(label=_('Telefono'), required=False)
+    phone = ITPhoneNumberField(label=_('Phone'), required=False)
 
     class Meta:
-	model = Staff
-	widgets = {'birth_date': widgets.AdminDateWidget(),
-		   'employ_date': widgets.AdminDateWidget(),
-		   }
-	fields = (
-		  'surname',
-		  'name',
-		  'birth_date',
-		  'gender',
-		  'nationality',
-		  'employ_date',
-		  'collagreement',
-		  'health_surveillance',
-		  'workers_count',
-		  'standard_task',
-		  'department',
-		  'role',
-		  )
+        model = Staff
+        widgets = {'birth_date': widgets.AdminDateWidget(),
+                   'employ_date': widgets.AdminDateWidget(),
+                   }
+        fields = (
+                  'surname',
+                  'name',
+                  'birth_date',
+                  'gender',
+                  'nationality',
+                  'employ_date',
+                  'collagreement',
+                  'health_surveillance',
+                  'workers_count',
+                  'standard_task',
+                  'department',
+                  'role',
+                  )
 
 class WorkingEnvironmentForm(forms.ModelForm):
     class Meta:
-	model = CustomerCompany
-	fields = ('working_environment', )
+        model = CustomerCompany
+        fields = ('working_environment', )
 
 class DepartmentForm(forms.ModelForm):
     class Meta:
-	model = Department
-	exclude = ('company', 'record_by', 'lastupdate_by')
+        model = Department
+        exclude = ('company', 'record_by', 'lastupdate_by')
 
 class CompanySecurityDutyForm(forms.ModelForm):
-    internal_phone = ITPhoneNumberField(label=_('Telefono interno'))
-    external_phone = ITPhoneNumberField(label=_('Telefono esterno'))
+    internal_phone = ITPhoneNumberField(label=_('Internal phone'))
+    external_phone = ITPhoneNumberField(label=_('External phone'))
 
     class Meta:
-	model = CompanySecurityDuty
-	exclude = ('company', 'record_by', 'lastupdate_by')
+        model = CompanySecurityDuty
+        exclude = ('company', 'record_by', 'lastupdate_by')
