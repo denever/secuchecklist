90eddbdf2c19c60b20c9532b74c41e63543fe0fb
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-04-04 14:31:23 +0200

    Aggiunto comando stampa dei riskfactors

---
+++ checklists/management/commands/print_riskfactor_tree.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+from checklists.models import RiskFactor
+
+
+class Command(BaseCommand):
+    args = '<filename.mdb>'
+    help = 'Import data from mdb files'
+
+    def print_riskfactor(self, rf, level=0):
+        print '-'*level, rf
+        for subrf in rf.children.all():
+            self.print_riskfactor(subrf, level + 2)
+
+    def handle(self, *args, **options):
+        for rf in RiskFactor.objects.filter(parent__exact=None):
+            self.print_riskfactor(rf)
