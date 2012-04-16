e64ddff53d49828147ed1b0e7b5d8a4df24f5aa5
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-04-16 10:21:11 +0200

    Aggiunt Root risk factor

--- riskfactors/management/commands/import_riskfactors.py
+++ riskfactors/management/commands/import_riskfactors.py
@@ -27,36 +27,37 @@ class Command(BaseCommand):
     help = 'Import data from mdb files'
 
     def handle(self, *args, **options):
-        DBfile = os.path.abspath(args[0])
-        connect_str = 'DRIVER={MDBTools};DBQ=%s' % DBfile
-        conn = pyodbc.connect(connect_str)
-        src_cur = conn.cursor()
+	DBfile = os.path.abspath(args[0])
+	connect_str = 'DRIVER={MDBTools};DBQ=%s' % DBfile
+	conn = pyodbc.connect(connect_str)
+	src_cur = conn.cursor()
 
-        src_cur.execute(QUERY_IMPORT)
-        for desc in src_cur:
-            codice_padre = str(desc[9]).split('/')[int(desc[10])]
-            obj = RiskFactor(description=desc[0],
-                             question=desc[1],
-                             measure=desc[2],
-                             suggestion_yes=desc[3],
-                             suggestion_no=desc[4],
-                             notes=desc[5],
-                             link=desc[6],
-                             filename=desc[7],
-                             code=desc[8].strip('/'),
-                             parent_code=codice_padre.strip('/'))
-            obj.save()
+	src_cur.execute(QUERY_IMPORT)
+	for desc in src_cur:
+	    codice_padre = str(desc[9]).split('/')[int(desc[10])]
+	    obj = RiskFactor(description=desc[0],
+			     question=desc[1],
+			     measure=desc[2],
+			     suggestion_yes=desc[3],
+			     suggestion_no=desc[4],
+			     notes=desc[5],
+			     link=desc[6],
+			     filename=desc[7],
+			     code=desc[8].strip('/'),
+			     parent_code=codice_padre.strip('/'))
+	    obj.save()
 
-        for risk_factor in RiskFactor.objects.all():
-            if risk_factor.parent_code != '':
-                risk_factor.parent = RiskFactor.objects.get(code=risk_factor.parent_code)
-                print '_'*10
-                print 'Child:', risk_factor
-                print 'Child code:', risk_factor.code
-                print 'Parent code:', risk_factor.parent_code
-                print 'Parent:', risk_factor.parent
-                print '#'*10
-            else:
-                print 'No father for', risk_factor
-                risk_factor.parent = None
-            risk_factor.save()
+	root_rf = RiskFactor(id=0, description='Fattori di Rischio', parent=None)
+	for risk_factor in RiskFactor.objects.all():
+	    if risk_factor.parent_code != '':
+		risk_factor.parent = RiskFactor.objects.get(code=risk_factor.parent_code)
+		print '_'*10
+		print 'Child:', risk_factor
+		print 'Child code:', risk_factor.code
+		print 'Parent code:', risk_factor.parent_code
+		print 'Parent:', risk_factor.parent
+		print '#'*10
+	    else:
+		print 'No father for', risk_factor
+		risk_factor.parent = root_rf
+	    risk_factor.save()
