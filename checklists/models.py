69608504c65e11c5cf303359a4c657fb20c6f36f
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-04-20 11:57:00 +0200

    Prima bozza del modello per la checklist

--- checklists/models.py
+++ checklists/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import ugettext as _
+
+# depends on django-simple-history
+# from simple_history.models import HistoricalRecords
 
 # Create your models here.
+class CheckList(models.Model):
+    company = models.ForeignKey('customers.CustomerCompany')
+
+    history = HistoricalRecords()
+
+    record_by = models.ForeignKey('accounts.UserProfile',
+                                  related_name='checklists_created',
+                                  verbose_name=_('Recorded by'))
+    lastupdate_by = models.ForeignKey('accounts.UserProfile',
+                                      related_name='checklists_edited',
+                                      verbose_name=_('Last update by'))
+    record_date = models.DateTimeField(_('Recorded on'), auto_now_add=True)
+
+class RiskFactorEvaluation(models.Model):
+    checklist = models.ForeignKey(CheckList)
+    risk_factor = models.ForeignKey('riskfactors.RiskFactor')
+    answer = models.BooleanField(_('Answer'))
+
+    record_by = models.ForeignKey('accounts.UserProfile',
+                                  related_name='evaluations_created',
+                                  verbose_name=_('Recorded by'))
+    lastupdate_by = models.ForeignKey('accounts.UserProfile',
+                                      related_name='evaluations_edited',
+                                      verbose_name=_('Last update by'))
+    record_date = models.DateTimeField(_('Recorded on'), auto_now_add=True)
