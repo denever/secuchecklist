14306a5cd3e2363c6437a2be249aa92e52a7eefd
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-04-20 16:18:51 +0200

    Utilizzata admin revision per checklists

--- checklists/admin.py
+++ checklists/admin.py
@@ -1,13 +1,14 @@
 from checklists.models import CheckList, RiskFactorEvaluation
 
 from django.contrib import admin
+import reversion
 
-# class CheckListAdmin(admin.ModelAdmin):
-#     list_display = ('description',)
-#     list_filter = ['description']
-#     search_fields = ['description', 'question', 'measure', 'link']
-#     date_hierarchy = 'record_date'
+class CheckListAdmin(reversion.VersionAdmin):
+    # list_display = ('description',)
+    # list_filter = ['description']
+    # search_fields = ['description', 'question', 'measure', 'link']
+    # date_hierarchy = 'record_date'
+    pass
 
-
-admin.site.register(CheckList)
+admin.site.register(CheckList, CheckListAdmin)
 admin.site.register(RiskFactorEvaluation)
