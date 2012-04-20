ffd8df41e0239010fb297be4859227ce335c94e5
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-04-20 19:36:44 +0200

    Rename CheckList in Checklist

--- checklists/admin.py
+++ checklists/admin.py
@@ -1,14 +1,14 @@
-from checklists.models import CheckList, RiskFactorEvaluation
+from checklists.models import Checklist, RiskFactorEvaluation
 
 from django.contrib import admin
 import reversion
 
-class CheckListAdmin(reversion.VersionAdmin):
+class ChecklistAdmin(reversion.VersionAdmin):
     # list_display = ('description',)
     # list_filter = ['description']
     # search_fields = ['description', 'question', 'measure', 'link']
     # date_hierarchy = 'record_date'
     pass
 
-admin.site.register(CheckList, CheckListAdmin)
+admin.site.register(Checklist, ChecklistAdmin)
 admin.site.register(RiskFactorEvaluation)
