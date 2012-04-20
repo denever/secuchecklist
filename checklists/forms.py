561d793ae1cc651395920976bc091d3fae9aa424
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-04-20 19:37:06 +0200

    Rinominato Checklist ed esclusi alcuni campi

--- checklists/forms.py
+++ checklists/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from checklists.models import CheckList, RiskFactorEvaluation
+from checklists.models import Checklist, RiskFactorEvaluation
 
-class CheckListForm(forms.ModelForm):
+class ChecklistForm(forms.ModelForm):
     class Meta:
-        model = CheckList
-
+        model = Checklist
+        exclude = ('company', 'record_by', 'lastupdate_by')
 
 class RiskFactorEvaluationForm(forms.ModelForm):
     class Meta:
