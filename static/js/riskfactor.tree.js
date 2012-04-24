2289a5a867b5dfd687be414e0542858beb599029
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-04-24 11:18:33 +0200

    Aggiunto buildevaldocument

--- static/js/riskfactor.tree.js
+++ static/js/riskfactor.tree.js
@@ -1,3 +1,11 @@
+function beforeSubmit(formData, jqForm, options){
+    console.log('Sottomettiamola');
+}
+
+function showResponse(responseText, statusText, xhr, $form){
+    console.log(statusText);
+}
+
 function buildtree(root_id){
     $('#riskfactors').jstree({
 				 "plugins": ["themes",
@@ -71,6 +79,73 @@ function buildtree(root_id){
 						  }
 						 }
 					 },
-				     }
-				 });
+				     },
+				 "type": {
+				     "valid_children": "folder",
+				     "types": {
+					 "default": {
+					     "valid_children": "none",
+					     "icon": "leaf",
+					 },
+					 "folder": {
+					     "valid_children": ["default", "folder"],
+					     "icon": {
+						 "image" : "http://static.jstree.com/v.1.0rc/_docs/_drive.png"
+					     }
+					 }
+				     },
+				 },
+			     });
 }
+
+function buildevaldocument(){
+    $('#riskfactors').jstree({
+				 "plugins": ["themes",
+					     "json",
+					     "ui",
+					     "cookies",
+					     "dnd",
+					     "types",
+					     "search",
+					     "hotkeys",
+					    ],
+				 "json": {
+				     "ajax": {
+					 "url" : function(n){
+					     if(n==-1){
+						 return "/riskfactors/node/0/";
+					     }
+					     // if(n.data){
+					     //		 console.log(n.children());
+					     //		 console.log(n.data('id'));
+					     //		 return "/riskfactors/"+n.data('id')+"/subtree/";
+					     // }
+					 },
+					 progressive_render: true,
+				     },
+				 },
+				 "search": {
+				     "ajax" : {
+					 "url" : "/riskfactors/search/",
+					 "data": function (str){
+					     return {
+						 "operation": "search",
+						 "search_str": "str"
+					     };
+					 }
+				     }
+				 },
+			     }).bind("select_node.jstree", function (event, data){
+					 $.get("eval/" + data.rslt.obj.data("id"),
+					       function(data){
+						   $("#riskfactorevaluation").html(data);
+						   $("#riskevalform").ajaxForm({
+										   beforeSubmit: beforeSubmit,
+										   success: showResponse,
+										   type: 'post'
+										   });
+						   });
+					 });
+}
+
+
