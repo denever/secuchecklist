813e57349d0ff8d4e0bb7ac90398f377cacdf15f
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-05-15 11:07:00 +0200

    Aggiunti url per post ajax di check,uncheck ed evals

--- risksevaluation/urls.py
+++ risksevaluation/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.auth.decorators import login_required, permission_required
+from django.views.decorators.csrf import csrf_exempt
 
 from risksevaluation.views import RisksEvaluationDocumentDetailView
 from risksevaluation.views import RisksEvaluationDocumentCreateView
@@ -8,7 +9,8 @@ from risksevaluation.views import RisksEvaluationDocumentDeleteView
 from risksevaluation.views import RisksEvaluationDocumentListView
 from risksevaluation.views import AllRisksEvaluationDocumentView
 from risksevaluation.views import RiskFactorEvaluationView
-
+from risksevaluation.views import UncheckRiskFactorView, CheckRiskFactorView
+from risksevaluation.views import EvalRiskFactorProbabilityView, EvalRiskFactorSeriousnessView
 # red stands for Risks Evaluation Document
 
 urlpatterns = patterns('risksevaluation.views',
@@ -39,4 +41,20 @@ urlpatterns = patterns('risksevaluation.views',
                        url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/eval/(?P<riskfactor>\d+)/$',
                            login_required(RiskFactorEvaluationView.as_view()),
                            name = 'eval'),
+
+                       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/uncheck$',
+                           login_required(csrf_exempt(UncheckRiskFactorView.as_view())),
+                           name = 'uncheck'),
+
+                       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/check$',
+                           login_required(csrf_exempt(CheckRiskFactorView.as_view())),
+                           name = 'check'),
+
+                       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/probeval$',
+                           login_required(csrf_exempt(EvalRiskFactorProbabilityView.as_view())),
+                           name = 'eval-probability'),
+
+                       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/sereval$',
+                           login_required(csrf_exempt(EvalRiskFactorSeriousnessView.as_view())),
+                           name = 'eval-seriousness'),
                        )
