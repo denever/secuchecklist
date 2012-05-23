8c59101571553c781c5792f8f25dc06d114a0309
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-05-23 17:46:29 +0200

    Commentato momentaneamente il middleware revision perché gestita manualmente la creazione delle revisioni

--- settings.py
+++ settings.py
@@ -102,6 +102,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+#    'reversion.middleware.RevisionMiddleware',
 )
 
 ROOT_URLCONF = 'secuchecklist.urls'
