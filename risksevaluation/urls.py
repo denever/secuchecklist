01b6ba7fd3819c2594be2fd01f3b746776d5af1c
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-05-25 15:55:11 +0200

    Whitespace cleanup

--- risksevaluation/urls.py
+++ risksevaluation/urls.py
@@ -15,51 +15,51 @@ from risksevaluation.views import UntakeMeasureView, TakeMeasureView
 # red stands for Risks Evaluation Document
 
 urlpatterns = patterns('risksevaluation.views',
-                       url(r'^$', login_required(AllRisksEvaluationDocumentView.as_view()),
-                           name='red'),
+		       url(r'^$', login_required(AllRisksEvaluationDocumentView.as_view()),
+			   name='red'),
 
-                       url(r'^company/(?P<company>\d+)$', login_required(RisksEvaluationDocumentListView.as_view()),
-                           name='red-list'),
+		       url(r'^company/(?P<company>\d+)$', login_required(RisksEvaluationDocumentListView.as_view()),
+			   name='red-list'),
 
-                       url(r'^(?P<company>\d+)/revision/(?P<pk>\d+)/$',
-                           login_required(RisksEvaluationDocumentDetailView.as_view()),
-                           name = 'red-detail'),
+		       url(r'^(?P<company>\d+)/revision/(?P<pk>\d+)/$',
+			   login_required(RisksEvaluationDocumentDetailView.as_view()),
+			   name = 'red-detail'),
 
-                       url(r'^(?P<company>\d+)/create/$',
-                           login_required(RisksEvaluationDocumentCreateView.as_view()),
-                           name = 'red-create'
-                           ),
+		       url(r'^(?P<company>\d+)/create/(?P<change>\d+)/$',
+			   login_required(RisksEvaluationDocumentCreateView.as_view()),
+			   name = 'red-create'
+			   ),
 
-                       url(r'^(?P<company>\d+)/edit/(?P<pk>\d+)/$',
-                           login_required(RisksEvaluationDocumentUpdateView.as_view()),
-                           name = 'red-edit'
-                           ),
+		       url(r'^(?P<company>\d+)/edit/(?P<pk>\d+)/$',
+			   login_required(RisksEvaluationDocumentUpdateView.as_view()),
+			   name = 'red-edit'
+			   ),
 
-                       url(r'^(?P<company>\d+)/delete/(?P<pk>\d+)/$',
-                           login_required(RisksEvaluationDocumentDeleteView.as_view()),
-                           name = 'red-delete'
-                           ),
-                       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/eval/(?P<riskfactor>\d+)/$',
-                           login_required(RiskFactorEvaluationView.as_view()),
-                           name = 'eval'),
+		       url(r'^(?P<company>\d+)/delete/(?P<pk>\d+)/$',
+			   login_required(RisksEvaluationDocumentDeleteView.as_view()),
+			   name = 'red-delete'
+			   ),
+		       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/eval/(?P<riskfactor>\d+)/$',
+			   login_required(RiskFactorEvaluationView.as_view()),
+			   name = 'eval'),
 
-                       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/status$',
-                           login_required(csrf_exempt(EvalRiskFactorStatusView.as_view())),
-                           name = 'status'),
+		       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/status$',
+			   login_required(csrf_exempt(EvalRiskFactorStatusView.as_view())),
+			   name = 'status'),
 
-                       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/probeval$',
-                           login_required(csrf_exempt(EvalRiskFactorProbabilityView.as_view())),
-                           name = 'eval-probability'),
+		       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/probeval$',
+			   login_required(csrf_exempt(EvalRiskFactorProbabilityView.as_view())),
+			   name = 'eval-probability'),
 
-                       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/sereval$',
-                           login_required(csrf_exempt(EvalRiskFactorSeriousnessView.as_view())),
-                           name = 'eval-seriousness'),
+		       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/sereval$',
+			   login_required(csrf_exempt(EvalRiskFactorSeriousnessView.as_view())),
+			   name = 'eval-seriousness'),
 
-                       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/take$',
-                           login_required(csrf_exempt(TakeMeasureView.as_view())),
-                           name = 'take'),
+		       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/take$',
+			   login_required(csrf_exempt(TakeMeasureView.as_view())),
+			   name = 'take'),
 
-                       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/untake$',
-                           login_required(csrf_exempt(UntakeMeasureView.as_view())),
-                           name = 'untake'),
-                       )
+		       url(r'^(?P<company>\d+)/revision/(?P<revision>\d+)/untake$',
+			   login_required(csrf_exempt(UntakeMeasureView.as_view())),
+			   name = 'untake'),
+		       )
