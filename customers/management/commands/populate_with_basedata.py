3f97d7ee7ff87d1fa90b71cf80ddf73417b49c81
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-05-25 15:45:00 +0200

    Whitespace cleanup

--- customers/management/commands/populate_with_basedata.py
+++ customers/management/commands/populate_with_basedata.py
@@ -15,25 +15,25 @@ class Command(BaseCommand):
     help = 'Populate db with utility data for Nationality, SecurityDuty'
 
     def handle(self, *args, **options):
-        duties = [_('Datore di lavoro'),
-                  _('Datore di lavoro con funzioni RSPP'),                  
-                  _('RSPP'),
-                  _('Dirigente'),
-                  _('Preposto'),
-                  _('RLS'),
-                  _('ASPP'),
-                  _('Medico competente'),
-                  _('Addetto al primosoccorso'),
-                  _('Adetto alle emergenze/antincendio')
-                  ]
+	duties = [_('Datore di lavoro'),
+		  _('Datore di lavoro con funzioni RSPP'),
+		  _('RSPP'),
+		  _('Dirigente'),
+		  _('Preposto'),
+		  _('RLS'),
+		  _('ASPP'),
+		  _('Medico competente'),
+		  _('Addetto al primosoccorso'),
+		  _('Adetto alle emergenze/antincendio')
+		  ]
 
-        for duty in duties:
-            obj = SecurityDuty(name=duty, description=duty)
-            obj.save()
+	for duty in duties:
+	    obj = SecurityDuty(name=duty, description=duty)
+	    obj.save()
 
-        obj = Nationality(nationality=_('Italiana'))
-        obj.save()
+	obj = Nationality(nationality=_('Italiana'))
+	obj.save()
 
-        u = User.objects.get(id=1)
-        up = UserProfile(user=u)
-        up.save()
+	u = User.objects.get(id=1)
+	up = UserProfile(user=u)
+	up.save()
