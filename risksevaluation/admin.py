be7e9750261adb378665fbb62adfabb66ef61b46
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-06-16 17:54:17 +0200

    Rimossi riferimenti a revision

--- risksevaluation/admin.py
+++ risksevaluation/admin.py
@@ -3,8 +3,8 @@ from risksevaluation.models import RisksEvaluationDocument, RiskFactorEvaluation
 from django.contrib import admin
 
 class RisksEvaluationDocumentAdmin(admin.ModelAdmin):
-    list_display = ('revision', 'record_date', 'revision_description', 'record_by')
-    list_filter = ['revision', 'record_date']
+    list_display = ('record_date', 'revision_description', 'record_by')
+    list_filter = ['record_date']
     search_fields = ['revision_description']
     date_hierarchy = 'record_date'
 
