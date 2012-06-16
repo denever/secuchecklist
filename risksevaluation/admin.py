b1129247bde5aa112779040193a3f51281400a51
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-06-16 17:42:40 +0200

    Rimossi riferimenti a reversion

--- risksevaluation/admin.py
+++ risksevaluation/admin.py
@@ -1,9 +1,8 @@
 from risksevaluation.models import RisksEvaluationDocument, RiskFactorEvaluation
 
 from django.contrib import admin
-import reversion
 
-class RisksEvaluationDocumentAdmin(reversion.VersionAdmin):
+class RisksEvaluationDocumentAdmin(admin.ModelAdmin):
     list_display = ('revision', 'record_date', 'revision_description', 'record_by')
     list_filter = ['revision', 'record_date']
     search_fields = ['revision_description']
