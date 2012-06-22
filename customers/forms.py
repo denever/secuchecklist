92dd1efb6327e6d8c929daf78f251a5fdc70f8f6
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-06-22 17:16:28 +0200

    Whitespace cleanup, exclusi record_by e lastupdate_by dalla equipment form

--- customers/forms.py
+++ customers/forms.py
@@ -22,74 +22,75 @@ class CustomerCompanyForm(forms.ModelForm):
     fax = ITPhoneNumberField(label=_('Fax'))
 
     class Meta:
-	model = CustomerCompany
-	fields = (
-	    'firm',
-	    'year_beginactivity',
-	    'phone',
-	    'fax',
-	    'email',
-	    'registered_office',
-	    'settlement',
-	    'ciiaa',
-	    'tax_code',
-	    'vat_code',
-	    'inail_pos',
-	    'inps_pos',
-	    'ccnl',
-	    'ateco_sector',
-	    'certifications',
-	    'settlement_size',
-	    'cpi',
-	    'machine_use',
-	    'dangerous_substances',
-	    'health_surveillance')
+        model = CustomerCompany
+        fields = (
+            'firm',
+            'year_beginactivity',
+            'phone',
+            'fax',
+            'email',
+            'registered_office',
+            'settlement',
+            'ciiaa',
+            'tax_code',
+            'vat_code',
+            'inail_pos',
+            'inps_pos',
+            'ccnl',
+            'ateco_sector',
+            'certifications',
+            'settlement_size',
+            'cpi',
+            'machine_use',
+            'dangerous_substances',
+            'health_surveillance')
 
-	widgets = {'ateco_sector': autocomplete_widgets.AutocompleteWidget('atecosector'),
-		   'cpi': autocomplete_widgets.AutocompleteWidget('cpisettlement')
-		   }
+        widgets = {'ateco_sector': autocomplete_widgets.AutocompleteWidget('atecosector'),
+                   'cpi': autocomplete_widgets.AutocompleteWidget('cpisettlement')
+                   }
 
 class StaffForm(forms.ModelForm):
     phone = ITPhoneNumberField(label=_('Phone'), required=False)
 
     class Meta:
-	model = Staff
-	widgets = {'birth_date': widgets.AdminDateWidget(),
-		   'employ_date': widgets.AdminDateWidget(),
-		   }
-	fields = (
-		  'surname',
-		  'name',
-		  'birth_date',
-		  'gender',
-		  'nationality',
-		  'employ_date',
-		  'collagreement',
-		  'health_surveillance',
-		  'workers_count',
-		  'standard_task',
-		  'department',
-		  'role',
-		  )
+        model = Staff
+        widgets = {'birth_date': widgets.AdminDateWidget(),
+                   'employ_date': widgets.AdminDateWidget(),
+                   }
+        fields = (
+                  'surname',
+                  'name',
+                  'birth_date',
+                  'gender',
+                  'nationality',
+                  'employ_date',
+                  'collagreement',
+                  'health_surveillance',
+                  'workers_count',
+                  'standard_task',
+                  'department',
+                  'role',
+                  )
 
 class WorkingEnvironmentForm(forms.ModelForm):
     class Meta:
-	model = CustomerCompany
-	fields = ('working_environment', )
+        model = CustomerCompany
+        fields = ('working_environment', )
 
 class DepartmentForm(forms.ModelForm):
     class Meta:
-	model = Department
-	exclude = ('company', 'record_by', 'lastupdate_by')
+        model = Department
+        exclude = ('company', 'record_by', 'lastupdate_by')
 
 class CompanySecurityDutyForm(forms.ModelForm):
     internal_phone = ITPhoneNumberField(label=_('Internal phone'))
     external_phone = ITPhoneNumberField(label=_('External phone'))
 
     class Meta:
-	model = CompanySecurityDuty
-	exclude = ('company', 'record_by', 'lastupdate_by')
+        model = CompanySecurityDuty
+        exclude = ('company', 'record_by', 'lastupdate_by')
 
 class EquipmentForm(forms.ModelForm):
     class Meta:
-	model = Equipment
+        model = Equipment
+        exclude = ('record_by', 'lastupdate_by')
