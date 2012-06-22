d8bc8760bd9a9509a3f45ed2013de57af9887edf
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-06-22 17:53:19 +0200

    Rinominato red_revision in revision, rimossa primary key e whitespace cleanup

--- risksevaluation/models.py
+++ risksevaluation/models.py
@@ -6,29 +6,29 @@ from django.utils.translation import ugettext as _
 # Create your models here.
 class RisksEvaluationDocument(models.Model):
     company = models.ForeignKey('customers.CustomerCompany')
-    red_revision = models.PositiveSmallIntegerField(_('Revision'), primary_key=True)
+    revision = models.PositiveSmallIntegerField(_('Revision'))
     revision_description = models.TextField(_('Revision description'))
     record_date = models.DateTimeField(_('Revision Date'), auto_now_add=True)
 
     record_by = models.ForeignKey('accounts.UserProfile',
-				  related_name='risksevaluationdocument_created',
-				  verbose_name=_('Recorded by'))
+                                  related_name='risksevaluationdocument_created',
+                                  verbose_name=_('Recorded by'))
     lastupdate_by = models.ForeignKey('accounts.UserProfile',
-				      related_name='risksevaluationdocument_edited',
-				      verbose_name=_('Last update by'))
+                                      related_name='risksevaluationdocument_edited',
+                                      verbose_name=_('Last update by'))
 
     rls_check = models.BooleanField(_('RLS check'))
     doctor_check = models.BooleanField(_('Doctor check'))
     companyowner_check = models.BooleanField(_('Company Owner check'))
 
     def __unicode__(self):
-	return '(%s %s)' % (self.red_revision, self.record_date)
+        return '(%s %s)' % (self.revision, self.record_date)
 
     class Meta:
-	ordering = ['record_date']
-	verbose_name = _('Risks Evaluation Document')
-	verbose_name_plural = _('Risks Evaluation Documents')
-	unique_together = ('company', 'record_date')
+        ordering = ['record_date']
+        verbose_name = _('Risks Evaluation Document')
+        verbose_name_plural = _('Risks Evaluation Documents')
+        unique_together = ('company', 'record_date', 'revision')
 
 probability = (
     (1, _('Low')),
@@ -56,11 +56,11 @@ class RiskFactorEvaluation(models.Model):
     risk_factor = models.ForeignKey('riskfactors.RiskFactor')
 
     record_by = models.ForeignKey('accounts.UserProfile',
-				  related_name='factorevaluations_created',
-				  verbose_name=_('Recorded by'))
+                                  related_name='factorevaluations_created',
+                                  verbose_name=_('Recorded by'))
     lastupdate_by = models.ForeignKey('accounts.UserProfile',
-				      related_name='factorevaluations_edited',
-				      verbose_name=_('Last update by'))
+                                      related_name='factorevaluations_edited',
+                                      verbose_name=_('Last update by'))
     record_date = models.DateTimeField(_('Recorded on'), auto_now_add=True)
 
     status = models.PositiveSmallIntegerField(_('Status'), choices=statuses)
@@ -70,10 +70,10 @@ class RiskFactorEvaluation(models.Model):
     measure_taken = models.BooleanField(_('Measure Taken'))
 
     class Meta:
-	ordering = ['record_date']
-	verbose_name = _('Risk Factor Evaluation')
-	verbose_name_plural = _('Risk Factor Evaluations')
-	unique_together = ('document','risk_factor')
+        ordering = ['record_date']
+        verbose_name = _('Risk Factor Evaluation')
+        verbose_name_plural = _('Risk Factor Evaluations')
+        unique_together = ('document','risk_factor')
 
     def __unicode__(self):
-	return '%s %s' % (self.risk_factor.description, _('Valued'))
+        return '%s %s' % (self.risk_factor.description, _('Valued'))
