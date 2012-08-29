53ec4c3f5c6a605a58a608a8b5a2dfb6df8b38d0
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-08-29 09:10:06 +0200

    Whitespace cleanup

--- customers/widgets.py
+++ customers/widgets.py
@@ -5,9 +5,7 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 from django.forms.fields import Field, RegexField, Select
 from django.contrib.localflavor.it.forms import ITZipCodeField
-
-# using autocomplete widgets
-from autocomplete import widgets as autocomplete_widgets
+from django.conf import settings
 
 phone_digits_re = re.compile(r'^((\+|00)39\s)?[0-9]{2,4}\s[0-9]{5,8}$')
 
@@ -22,69 +20,80 @@ class ITPhoneNumberField(Field):
     Spaces between prefixes and number are mandatory.
     """
     default_error_messages = {
-        'invalid':_(u'Enter a valid phone number'),
+	'invalid':_(u'Enter a valid phone number'),
     }
 
     def clean(self, value):
-        value = super(ITPhoneNumberField, self).clean(value)
-        if value == u'':
-            return value
+	value = super(ITPhoneNumberField, self).clean(value)
+	if value == u'':
+	    return value
 
-        n = phone_digits_re.search(value)
+	n = phone_digits_re.search(value)
 
-        if not n:
-            raise forms.ValidationError(self.error_messages['invalid'])
+	if not n:
+	    raise forms.ValidationError(self.error_messages['invalid'])
 
-        # Import a list of valid prefixes
-        from it_phones import PHONE_CHOICES, MOBILE_CHOICES
+	# Import a list of valid prefixes
+	from it_phones import PHONE_CHOICES, MOBILE_CHOICES
 
-        # Split the number and, if present, strip the international prefix from the number
-        m = value.split(" ")
+	# Split the number and, if present, strip the international prefix from the number
+	m = value.split(" ")
 
-        if (m[0] == '+39') or (m[0] == '0039'):
-            del m[0]
+	if (m[0] == '+39') or (m[0] == '0039'):
+	    del m[0]
 
-        # Check if the number is no longer than 11 digits
-        if (len(m[0]) + len(m[1])) > 11:
-            raise forms.ValidationError(self.error_messages['invalid'])
+	# Check if the number is no longer than 11 digits
+	if (len(m[0]) + len(m[1])) > 11:
+	    raise forms.ValidationError(self.error_messages['invalid'])
 
-        # Check the prefix against a list of valid fixed and mobile prefixes
-        if m[0] in PHONE_CHOICES or m[0] in MOBILE_CHOICES:
-            return u'%s %s' % (m[0], m[1])
+	# Check the prefix against a list of valid fixed and mobile prefixes
+	if m[0] in PHONE_CHOICES or m[0] in MOBILE_CHOICES:
+	    return u'%s %s' % (m[0], m[1])
 
-        raise forms.ValidationError(self.error_messages['invalid'])
+	raise forms.ValidationError(self.error_messages['invalid'])
 
 
 class AddressWidget(forms.MultiWidget):
-    def __init__(self, attrs=None):
-        widgets = (
-            forms.widgets.TextInput(attrs={'placeholder':'Via/Piazza'}),
-            forms.widgets.TextInput(attrs={'placeholder':'Numero Civico'}),
-            autocomplete_widgets.AutocompleteWidget('town', attrs={'placeholder':'Comune'}),
-            autocomplete_widgets.AutocompleteWidget('zipcode', attrs={'placeholder':'CAP'}),
-            autocomplete_widgets.AutocompleteWidget('province', attrs={'placeholder':'Provincia'}),
-            )
+    class Media:
+        js = (
+            'js/jquery.min.js',
+            'js/jquery-ui.min.js',
+            'js/jquery_autocomplete.js',
+            'js/capautocomplete.js'
+        )
+        css = {'all':
+            (settings.AUTOCOMPLETE_MEDIA_PREFIX + 'css/jquery-ui.css',)
+        }
 
-        super(AddressWidget, self).__init__(widgets, attrs)
+    def __init__(self, attrs=None):
+	widgets = (
+	    forms.widgets.TextInput(attrs={'placeholder':'Via/Piazza'}),
+	    forms.widgets.TextInput(attrs={'placeholder':'Numero Civico'}),
+	    forms.widgets.TextInput(attrs={'placeholder':'CAP'}),
+	    forms.widgets.TextInput(attrs={'placeholder':'Comune'}),
+	    forms.widgets.TextInput(attrs={'placeholder':'Provincia'}),
+	    )
+
+	super(AddressWidget, self).__init__(widgets, attrs)
 
     def decompress(self, value):
-        if value:
-            return value.decompress()
-        else:
-            return ['','','','','']
+	if value:
+	    return value.decompress()
+	else:
+	    return ['','','','','']
 
 class AddressFormField(forms.MultiValueField):
     widget = AddressWidget
 
     def __init__(self, *args, **kwargs):
-        fields = (
-            forms.CharField(label='Via/Piazza'),
-            forms.CharField(label='Numero Civico'),
-            forms.CharField(label='Comune'),
-            ITZipCodeField(label='CAP'),
-            forms.CharField(label='Provincia'),
-            )
-        super(AddressFormField, self).__init__(fields, *args, **kwargs)
+	fields = (
+	    forms.CharField(label='Via/Piazza'),
+	    forms.CharField(label='Numero Civico'),
+	    ITZipCodeField(label='CAP'),
+	    forms.CharField(label='Comune'),
+	    forms.CharField(label='Provincia'),
+	    )
+	super(AddressFormField, self).__init__(fields, *args, **kwargs)
 
     def compress(self, data_list):
-        return ','.join(data_list)
+	return ','.join(data_list)
